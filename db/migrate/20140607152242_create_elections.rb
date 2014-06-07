75da3a49f0afcb3ff394d0724644d7420cf87fdf
ka <ka.kaosf@gmail.com>
2014-06-08 00:23:50 +0900

    Deny null for election name and candidates_total columns

--- db/migrate/20140607152242_create_elections.rb
+++ db/migrate/20140607152242_create_elections.rb
@@ -1,8 +1,8 @@
 class CreateElections < ActiveRecord::Migration
   def change
     create_table :elections do |t|
-      t.string :name
-      t.integer :candidates_total
+      t.string :name,              null: false
+      t.integer :candidates_total, null: false
 
       t.timestamps
     end
