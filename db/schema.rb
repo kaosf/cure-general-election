a3976d2654928995218b94d0ff3655d06399efe6
ka <ka.kaosf@gmail.com>
2014-06-08 00:24:14 +0900

    Run "rake db:migrate"

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140607131754) do
+ActiveRecord::Schema.define(version: 20140607152242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "elections", force: true do |t|
+    t.string   "name",             null: false
+    t.integer  "candidates_total", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
