918829fa751ab23d6d5e55c2d6c3e332a8d1530f
ka <ka.kaosf@gmail.com>
2014-06-08 03:08:08 +0900

    Add routings for elections and votes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     :omniauth_callbacks => "users/omniauth_callbacks"
   }
+
+  resources :elections, only: [] do
+    resource :vote, only: [:create, :update]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
