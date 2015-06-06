4a200ebfdbc04400807427742890fcbeab9ab377
ka <ka.kaosf@gmail.com>
2015-06-06 21:41:37 +0900

    Create the new Election record and use it

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     if user_signed_in?
-      @election = Election.find 1
+      @election = Election.find 2
       @vote = @election.votes.find_by(user_id: current_user.id)
       @vote ||= @election.votes.build
     end
