97d51251b9b8b4caa9dd9f73e878219c79ef5186
ka <ka.kaosf@gmail.com>
2020-03-27 23:50:10 +0900

    Revert "Fix omniauth twitter configuration"
    This reverts commit 758649161ab65aef6cc3847095e5ef400b907e6f.

--- config/initializers/omniauth.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
-end
-# ref. https://github.com/arunagw/omniauth-twitter/blob/e3e61e0e7878aa97a11f3178b982591f8a14ad33/README.md
